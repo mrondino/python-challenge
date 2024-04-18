2e8d5ce81346800ac18013af5b800a2d1bf5bb72
mrondino <mrondino@nautique.com>
2024-04-18 17:23:46 -0400

    Created PyBank code and PyPoll code.

---
+++ PyBank/main.py
@@ -0,0 +1,74 @@
+#Import specific CSV file
+import os
+import csv
+csvpath = os.path.join("PyBank","Resources", "budget_data.csv")
+
+
+#Create empty string and variables
+month = []
+total_amount = 0
+running_total = 0
+previous_amount = 0
+difference = 0
+average_change = 0
+greatest_increase = 0
+greatest_decrease = 0
+greatest_increase_month = 0
+greatest_decrease_month = 0
+
+
+#Open the CSV file           
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+   #iterate through the CSV file
+    for row in csvreader:
+        #Zip the month list
+        month.append(row[0])
+        total_amount += int(row[1])
+        
+        #calculate the difference
+        if previous_amount != 0:
+            difference = ((int(row[1]))-(previous_amount))
+        
+        #store the greatest increase
+        if difference > greatest_increase:
+            greatest_increase = difference
+            greatest_increase_month = (row[0])
+
+        #store the greatest decrease
+        if difference < greatest_decrease:
+            greatest_decrease = difference
+            greatest_decrease_month = (row[0])
+        
+        #calculate the running total
+        running_total += difference           
+       
+       #set the previous amount for the next equation
+        previous_amount = int(row[1])  
+    
+    #calculate the average change
+    average_change = round((running_total/85),2)
+
+#print the outcome    
+print("Financial Analysis")
+print("-----------------------------------")
+
+print(f"Total Months: {len(month)}")
+print(f"Total: ${total_amount}")
+print(f"Average Change: {average_change}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
+
+#print the outcome to a txt file
+file_path = 'output.txt'
+
+with open(file_path, 'w') as file:
+    file.write("Financial Analysis\n"
+    "-----------------------------------\n"
+
+    f"Total Months: {len(month)}\n"
+    f"Total: ${total_amount}\n"
+    f"Average Change: {average_change}\n"
+    f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
---
+++ PyPoll/main.py
@@ -0,0 +1,66 @@
+#Import specific CSV file
+import os
+import csv
+csvpath = os.path.join("PyPoll","Resources", "election_data.csv")
+
+
+#Create empty string and variables
+votes = []
+CCS_List = []
+CCS_Percent = 0
+CCS_Total = 0
+DD_List = []
+DD_Percent = 0
+DD_Total = 0
+RAD_List = []
+RAD_Percent = 0
+RAD_Total = 0
+Winner = 0
+
+#Open the CSV file           
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+   #iterate through the CSV file
+    
+    #count votes for each candidate
+    for row in csvreader:
+        votes.append(row[0])
+        if row[2] == "Charles Casper Stockham":
+            CCS_List.append(row[2])
+        if row[2] == "Diana DeGette":
+            DD_List.append(row[2])
+        if row[2] == "Raymon Anthony Doane":
+            RAD_List.append(row[2])
+
+    #calculate candidate percentage by dividing by total votes
+    CCS_Percent = round((((len(CCS_List))/(len(votes)))*100),3)
+    DD_Percent = round((((len(DD_List))/(len(votes)))*100),3)
+    RAD_Percent = round((((len(RAD_List))/(len(votes)))*100),3)
+
+#print the outcome    
+print("Election Results")
+print("-----------------------------------")
+print(f"Total Votes: {len(votes)}")
+print("-----------------------------------")
+print(f"Charles Casper Stockham: {CCS_Percent}% ({len(CCS_List)})")
+print(f"Diana DeGette: {DD_Percent}% ({len(DD_List)})")
+print(f"Raymon Anthony Doane: {RAD_Percent}% ({len(RAD_List)})")
+print("-----------------------------------")
+print(f"Winner: {Winner}")
+print("-----------------------------------")
+
+#print the outcome to a txt file
+file_path = 'output.txt'
+
+with open(file_path, 'w') as file:
+    file.write("Election Results\n"
+        "-----------------------------------\n"   
+        f"Total Votes: {len(votes)}\n"
+        "-----------------------------------\n"
+        f"Charles Casper Stockham: {CCS_Percent}% ({len(CCS_List)})\n"
+        f"Diana DeGette: {DD_Percent}% ({len(DD_List)})\n"
+        f"Raymon Anthony Doane: {RAD_Percent}% ({len(RAD_List)})\n"
+        "-----------------------------------\n"
+        f"Winner: {Winner}\n"
+        "-----------------------------------\n")
